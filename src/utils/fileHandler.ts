a5ff3d7baeaf88be81fa688c3c391eec0c4da9a6
arthur <arthurmedeiros32@gmail.com>
2023-04-30 20:18:01 -0300

    feat: make it more user friendly and correctly add json support

--- src/utils/fileHandler.ts
+++ src/utils/fileHandler.ts
@@ -1,7 +1,18 @@
 import { readFileSync, writeFileSync } from "fs";
 
-export function saveTextsToFile(texts: string[], filename: string): void {
+export function saveTextsToTxtFile(texts: string[], filename: string): void {
   const fileContent = texts.join("\n");
+  writeFileSync(filename, fileContent, "utf8");
+}
+
+export function saveTextToJsonFile(texts: string[], filename: string): void {
+  const json = texts.map((text) => ({ question: text, answer: "" }));
+  const fileContent = JSON.stringify(json, null, 2);
+  writeFileSync(filename, fileContent, "utf8");
+}
+
+export function updateJsonFile(filename: string, data: any): void {
+  const fileContent = JSON.stringify(data, null, 2);
   writeFileSync(filename, fileContent, "utf8");
 }
 
